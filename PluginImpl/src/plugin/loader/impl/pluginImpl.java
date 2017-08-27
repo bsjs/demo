b59e058cac0c43504dfa2e765408a537248eec57
bsjs <wk_bupt@163.com>
2017-08-27 21:11:57 +0800

    pluginImpl

---
+++ PluginImpl/src/plugin/loader/impl/pluginImpl.java
@@ -0,0 +1,13 @@
+package plugin.loader.impl;
+
+import plugin.loader.plugin;
+
+public class pluginImpl implements plugin{
+
+	@Override
+	public void printPlugin() {
+		// TODO Auto-generated method stub
+		System.out.println("implement plugin");
+	}
+
+}
