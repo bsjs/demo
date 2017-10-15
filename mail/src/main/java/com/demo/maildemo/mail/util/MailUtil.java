9660b09e9f5bd7af34b4915b766ed88466fb101b
bsjs <wk_bupt@163.com>
2017-10-15 17:02:00 +0800

    邮件接收部分

--- mail/src/main/java/com/demo/maildemo/mail/util/MailUtil.java
+++ mail/src/main/java/com/demo/maildemo/mail/util/MailUtil.java
@@ -1,5 +1,14 @@
 package com.demo.maildemo.mail.util;
 
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -34,5 +43,31 @@ public class MailUtil {
         message.setSubject(titel);  
         message.setText(content);  
         mailSender.send(message);  
-    }  
+    } 
+	
+	public int reciveMail() throws MessagingException
+	{
+		//配置连接协议
+		Properties properties = new Properties();
+		properties.setProperty("mail.pop3.host", mailEntity.getMailHost());
+		properties.setProperty("mail.store.protocol", mailEntity.getProtocol());
+		
+		//获取一个会话
+		Session session = Session.getInstance(properties);
+		session.setDebug(true);
+		
+		//打开邮件文件夹
+		Store store = session.getStore();
+		store.connect(mailEntity.getMailHost(),mailEntity.getUserName(),mailEntity.getPassWord());
+		
+		//获取邮箱内的邮件夹Floder对象，以“只读”打开
+		Folder folder = store.getFolder("INBOX");
+		folder.open(Folder.READ_ONLY);
+		
+		//获取邮件夹Folder内所有的邮件Message对象
+		Message [] messages = folder.getMessages();
+		
+		System.out.print(messages.length);
+		return messages.length;
+	}
 }
